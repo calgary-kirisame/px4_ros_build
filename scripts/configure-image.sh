1fd1ff86c1bde4c9d522f44da77e17b9021afa67
calgary-kirisame <131201352+calgary-kirisame@users.noreply.github.com>
2026-05-03 19:51:18 -0400

    oops

--- scripts/configure-image.sh
+++ scripts/configure-image.sh
@@ -40,7 +40,7 @@ systemd-nspawn --pipe -D "$MNT" ldconfig
 systemd-nspawn --pipe -D "$MNT" --bind-ro=/etc/resolv.conf bash -c '
   set -euo pipefail
   apt-get update
-  apt-get install -y --no-install-recommends python3-rosdep can-utils
+  apt-get install -y --no-install-recommends python3-rosdep2 can-utils
   rosdep init
   rosdep update --rosdistro=jazzy
   rosdep install --from-paths /opt/ros/jazzy/share -i -y --rosdistro=jazzy \
